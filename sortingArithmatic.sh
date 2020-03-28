6168d9a02fa7763d6e90804a211257a0ec0255b4
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-29 00:02:48 +0530

    ADDED: modAdd function

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -8,9 +8,13 @@ function multiplyAdd() {
 function addDivide() {
 	echo $(($(($1+$2)) / $3))
 }
+function modAdd() {
+	echo $(($(($1%$2)) + $3))
+}
 echo "Enter three numbers"
 read a b c 
 #echo $a $b $c
 addMultiply $a $b $c
 multiplyAdd $a $b $c
 addDivide $a $b $c
+modAdd $a $b $c
