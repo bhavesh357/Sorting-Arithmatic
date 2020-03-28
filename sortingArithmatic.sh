1ed9855471836f16880189f37b32911ccbc43f07
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-29 00:35:29 +0530

    ADDED: array to store values

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+#Constants
+declare -A values
+valuesInArray
 function addMultiply() {
 	echo $(($(($1+$2)) * $3))
 }
@@ -11,8 +14,7 @@ function addDivide() {
 function modAdd() {
 	echo $(($(($1%$2)) + $3))
 }
-#Constants
-declare -A values
+
 echo "Enter three numbers"
 read a b c 
 #echo $a $b $c
@@ -20,5 +22,7 @@ values["addMultiply"]=$(addMultiply $a $b $c)
 values["multiplyAdd"]=$(multiplyAdd $a $b $c)
 values["addDivide"]=$(addDivide $a $b $c)
 values["modAdd"]=$(modAdd $a $b $c)
-echo ${!values[@]}
-echo ${values[@]}
+#echo ${!values[@]}
+#echo ${values[@]}
+valuesInArray=${values[@]}
+#echo $valuesInArray
