801c9ad249be8161e0a38a3442c17eab7e21c96e
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-28 23:58:37 +0530

    ADDED: addDivide function

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -5,7 +5,12 @@ function addMultiply() {
 function multiplyAdd() {
 	echo $(($(($1*$2)) + $3))
 }
+function addDivide() {
+	echo $(($(($1+$2)) / $3))
+}
 echo "Enter three numbers"
 read a b c 
 #echo $a $b $c
 addMultiply $a $b $c
+multiplyAdd $a $b $c
+addDivide $a $b $c
