ad626da9f1ae485ed28d30418d1c8eca04bc5ed6
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-29 12:36:44 +0530

    ADDED: sort function which does both ascending and descending sorting

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -2,6 +2,8 @@
 #Constants
 declare -A values
 declare -a valuesInArray
+declare -a ascendingArray
+declare -a descendingArray
 function addMultiply() {
 	echo $(($(($1+$2)) * $3))
 }
@@ -14,70 +16,57 @@ function addDivide() {
 function modAdd() {
 	echo $(($(($1%$2)) + $3))
 }
-function sortDescending() {
-	for ((i=0;i<4;i++))
-	do
-		Largest=${valuesInArray[i]}
-		indexOfLargest=$i
-		for ((j=$i;j<4;j++))
-		do
-		#finding largest from i index
-		if [ ${valuesInArray[j]} -gt $Largest ]
-		then
-			Largest=${valuesInArray[j]}
-			indexOfLargest=$j
-		fi
-	done
-	#swapping the numbers
-	temp=${valuesInArray[i]}
-	valuesInArray[i]=${valuesInArray[indexOfLargest]}
-	valuesInArray[indexOfLargest]=$temp
-done
-}
-function sortAscending() {
-	for ((i=0;i<4;i++))
-	do
-		Smallest=${valuesInArray[i]}
-		indexOfSmallest=$i
-		for ((j=$i;j<4;j++))
-		do
-		#finding smallest from i index
-		if [ ${valuesInArray[j]} -lt $Smallest ]
-		then
-			Largest=${valuesInArray[j]}
-			indexOfSmallest=$j
-		fi
-	done
-	#swapping the numbers
-	temp=${valuesInArray[i]}
-	valuesInArray[i]=${valuesInArray[indexOfSmallest]}
-	valuesInArray[indexOfSmallest]=$temp
-done
-}
 function performOperations() {
 	values["addMultiply"]=$(addMultiply $1 $2 $3)
 	values["multiplyAdd"]=$(multiplyAdd $1 $2 $3)
 	values["addDivide"]=$(addDivide $1 $2 $3)
 	values["modAdd"]=$(modAdd $1 $2 $3)
-	#echo ${!values[@]}
-	#echo ${values[@]}`
 }
 function storeInArray() {
 	valuesInArray[0]=${values["addMultiply"]}
 	valuesInArray[1]=${values["multiplyAdd"]}
 	valuesInArray[2]=${values["addDivide"]}
 	valuesInArray[3]=${values["modAdd"]}
-	#echo $valuesInArray
+}
+function sort() {
+	for ((k=0;k<4;k++))
+	do
+		ascendingArray[k]=${valuesInArray[k]}
+		descendingArray[k]=${valuesInArray[k]}
+	done
+	for ((i=0;i<4;i++))
+	do
+		smallest=${ascendingArray[i]}
+		largest=${descendingArray[i]}
+		indexOfSmallest=$i
+		indexOfLargest=$i
+		for ((j=$i;j<4;j++))
+		do
+			#finding smallest from i index
+			if [ ${ascendingArray[j]} -lt $smallest ]
+			then
+				smallest=${ascendingArray[j]}
+				indexOfSmallest=$j
+			fi
+			if [ ${descendingArray[j]} -gt $largest ]
+			then
+				largest=${descendingArray[j]}
+				indexOfLargest=$j
+			fi
+		done
+		tempSmallest=${ascendingArray[i]}
+		ascendingArray[i]=${ascendingArray[indexOfSmallest]}
+		ascendingArray[indexOfSmallest]=$tempSmallest
+		tempLargest=${descendingArray[i]}
+		descendingArray[i]=${descendingArray[indexOfLargest]}
+		descendingArray[indexOfLargest]=$tempLargest
+	done	
 }
 echo "Enter three numbers"
 read a b c 
-#echo $a $b $c
 #perform all operations
 performOperations $a $b $c
-#store values in array
 storeInArray
-#sorting
-sortDescending 
-echo ${valuesInArray[@]}
-sortAscending 
-echo ${valuesInArray[@]}
+sort
+echo ${ascendingArray[@]}
+echo ${descendingArray[@]}
