0e156f2788ef13240129586867f9177987fd0a92
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-29 11:11:12 +0530

    ADDED: sortDescending function

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 #Constants
 declare -A values
-valuesInArray
+declare -a valuesInArray
 function addMultiply() {
 	echo $(($(($1+$2)) * $3))
 }
@@ -14,7 +14,26 @@ function addDivide() {
 function modAdd() {
 	echo $(($(($1%$2)) + $3))
 }
-
+function sortDescending() {
+	for ((i=0;i<4;i++))
+	do
+		Largest=${valuesInArray[i]}
+		indexOfLargest=$i
+		for ((j=$i;j<4;j++))
+		do
+		#finding largest from i index
+		if [ ${valuesInArray[j]} -gt $Largest ]
+		then
+			Largest=${valuesInArray[j]}
+			indexOfLargest=$j
+		fi
+	done
+	#swapping the numbers
+	temp=${valuesInArray[i]}
+	valuesInArray[i]=${valuesInArray[indexOfLargest]}
+	valuesInArray[indexOfLargest]=$temp
+done
+}
 echo "Enter three numbers"
 read a b c 
 #echo $a $b $c
@@ -23,6 +42,11 @@ values["multiplyAdd"]=$(multiplyAdd $a $b $c)
 values["addDivide"]=$(addDivide $a $b $c)
 values["modAdd"]=$(modAdd $a $b $c)
 #echo ${!values[@]}
-#echo ${values[@]}
-valuesInArray=${values[@]}
+#echo ${values[@]}`
+valuesInArray[0]=${values["addMultiply"]}
+valuesInArray[1]=${values["multiplyAdd"]}
+valuesInArray[2]=${values["addDivide"]}
+valuesInArray[3]=${values["modAdd"]}
 #echo $valuesInArray
+sortDescending 
+echo ${valuesInArray[@]}
